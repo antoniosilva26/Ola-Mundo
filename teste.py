2f99c14270a39371354f27612124e6a15deea6f8
Toni Silva <baianodeposito9@gmail.com>
2026-05-16 00:51:38 -0300

    Coloquei uns mini testes
    Teste curso em video

--- teste.py
+++ teste.py
@@ -1,1 +1,9 @@
 print("Hello, World!")
+
+num1 = input('Digite um valor')
+print(f"Só tem espaços? {num1.isspace()}")
+print(f"Está em maiúsculo?? {num1.isupper()}")
+print(f"Está em minúsculo?? {num1.islower()}")
+print(f"Está alfabetizada?? {num1.isalpha()}")
+print(f"Está alfanumérico?? {num1.isalnum()}")
+print(f'O tipo desse valor é: {type(num1)}')
